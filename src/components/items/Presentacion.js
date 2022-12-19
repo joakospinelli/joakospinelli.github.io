efe2df80ac537df6f880bd4496c003486d099215
Joaquin Spinelli <joakospinelli@gmail.com>
2022-12-19 16:31:37 -0300

    cambios en el estilo de la página + nueva info en la presentación

--- src/components/items/Presentacion.js
+++ src/components/items/Presentacion.js
@@ -6,8 +6,6 @@ export default function Presentacion(){
         return Math.abs(edad.getUTCFullYear() - 1970);
     }
 
-    
-/*  ESTO SÓLO VA A ESTAR CUANDO ME LIBERE DE LA VERGUENZA  
     const changeImg = () => {
         const randomImg = [ "https://media.tenor.com/kB-Liucd8p4AAAAC/pepega-hackermans.gif",
                             "https://media.tenor.com/Z4Ks69TQz0wAAAAC/hacker-pepe.gif",
@@ -21,31 +19,33 @@ export default function Presentacion(){
             selected = randomImg[ Math.floor(Math.random() * randomImg.length) ];
 
         img.src = selected;
-    } */
+    }
 
     return (
         <div className="col-info last-item">
             <h3 className="subtitle-accent">PRESENTACIÓN</h3>
             <p>
                 Me llamo Joaquín, tengo {getEdad()} años y soy de La Plata.
-                Ahora estoy estudiando Analista en TICs en la Universidad Nacional de La Plata, cursando el tercer año;
+                Ahora estoy estudiando Analista en TICs en la Universidad Nacional de La Plata;
                 mi objetivo es anotarme en todas las carreras de la facultad (terminarlas es otro tema).
-                También hago cursos de Udemy y los dejo entre el 2% y 50%. Soy un entusiasta fundamentalista del tereré, 24/7 todo el año.
+                También hago cursos de Udemy y los dejo entre el 2% y 50%.
             </p>
 
             <p>
-                Empecé a programar en 2019, cuando hice un curso de programación que dieron en la facultad para estudiantes de secundaria,
-                y ahí decidí que iba a estudiar esto. Me interesa la programación orientada a objetos
-                y el desarrollo Web (tanto Frontend como Backend).
+                Empecé a programar en 2019, cuando hice un curso de programación que dieron en la facultad para estudiantes de secundaria.
+                Me interesa la programación orientada a objetos y el desarrollo Web.
             </p>
 
-{/*         ESTO SÓLO VA A ESTAR CUANDO ME LIBERE DE LA VERGUENZA
+            <p>
+                Me gustan los perros, dibujar y comer. Soy un entusiasta fundamentalista del tereré, 24/7 todo el año.
+            </p>
+
             <div style={{ textAlign: 'center '}} onMouseEnter={changeImg}>
                 <img src="https://media.tenor.com/ZmZ7UKIc0soAAAAC/anonymous-anonymous-bites-back.gif"
                             alt="rana con anteojos"
                             id="img-perfil"
                             className="img"></img>
-            </div> */}
+            </div>
 
         </div>
     )
